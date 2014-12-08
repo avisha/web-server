f1849a227098f367576902e7817cd3b060e2095a
unknown <avijain@avijain-W7-1.corp.adobe.com>
2014-12-08 12:25:21 +0530

    committed

--- src/main/java/com/adobe/web/server/HTTPWebServer.java
+++ src/main/java/com/adobe/web/server/HTTPWebServer.java
@@ -5,9 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
